053c09096ba69784318043970ec8f9949ea2cbee
linzifeng12138 <2582605401@qq.com>
2018-02-04 10:19:38 +0800

    添加首页轮播图插件效果

--- src/js/header.js
+++ src/js/header.js
@@ -1,8 +1,7 @@
 jQuery(function($){
     // 实现点击切换隐藏二级菜单栏
-    console.log($('#catelist h3'));
+  
     $('#catelist h3').on('click',function(){
-        console.log(222);
         $('#catelist .tablist1').slideToggle(250);
     });
 });
--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ jQuery(function($){
     //引入头部HTML文件,包括顶部导航栏，nav ，和产品列表菜单栏
     $('#pageHeader').load('html/header.html');
     //注意是以index.html为基准发起请求
-
+    
     // 图片偏移动画
     //注意给img样式设置绝对定位，以及父级元素设置相对定位
     $('.picframe').on('mouseenter','img',function(){
@@ -15,6 +15,10 @@ jQuery(function($){
             left:0
         },90);
     });
-    
-
+    //首页轮播图代码：
+    $('#banner').lzfbanner({
+        imgs:['img/ban1.jpg','img/ban2.jpg','img/ban3.jpg','img/ban4.jpg','img/ban5.png'],
+        buttons:false,
+        index:2
+    });
 });
