99a9bc0772226ee70b732f756186193d53a76c26
linzifeng12138 <2582605401@qq.com>
2018-02-06 16:33:52 +0800

    完成注册和登录页样式

---
+++ src/js/login.js
@@ -0,0 +1,10 @@
+/* 
+* @Author: Marte
+* @Date:   2018-02-06 12:53:49
+* @Last Modified by:   Marte
+* @Last Modified time: 2018-02-06 12:53:52
+*/
+
+$(document).ready(function(){
+    
+});
---
+++ src/js/reg.js
@@ -0,0 +1,10 @@
+/* 
+* @Author: Marte
+* @Date:   2018-02-06 12:54:00
+* @Last Modified by:   Marte
+* @Last Modified time: 2018-02-06 12:54:03
+*/
+
+$(document).ready(function(){
+    
+});
